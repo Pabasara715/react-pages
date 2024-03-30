41f2caf0a0929918ae379fa3a60112ec3a4ac6d7
Pabasara715 <pabasaravihanga715@gmail.com>
2024-03-30 11:05:16 +0530

    profile container

---
+++ reactpages/Screens/HomeScreen.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text } from "react-native";
+import ProfileCard from "../Components/ProfileCard";
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <ProfileCard />
+    </View>
+  );
+};
+
+export default HomeScreen;
